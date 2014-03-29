fa519cd62fe83204cc82efaabd27b78edbd48b54
Dan Lopuch <dan@datahero.com>
2014-03-29 12:25:15 -0700

    Initial require.js configuration

---
+++ public/js/config.js
@@ -0,0 +1,38 @@
+/**
+ * Require config
+ */
+require.config({
+
+  deps: ['main'],
+
+  baseUrl: "/js/",
+
+  paths: {
+    "jquery": "/lib/jquery-2.1.0.min",
+    "backbone": "/lib/backbone-min",
+    "d3": "/lib/d3.v3.3.2.min",
+    "bootstrap": "/lib/bootstrap_232/js/bootstrap.min",
+    "tpl": "/lib/tpl",
+    "lodash": "/lib/lodash.min",
+    "highcharts": "/lib/highcharts-all",
+    "jquery-slider": "/lib/jquery-ui-1.10.4.slider"
+  },
+
+  shim: {
+    bootstrap: {
+      deps: ['jquery']
+    },
+    d3: {
+      exports: "d3"
+    },
+    highcharts: {
+      deps: ['jquery'],
+      exports: "Highcharts"
+    },
+    "jquery-slider": {
+      deps: ['jquery'],
+      exports: "$"
+    }
+  }
+
+});
---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+require(['jquery'], function($) {
+  alert('require.js dependency management configured. \n\nLets get started.');
+});
