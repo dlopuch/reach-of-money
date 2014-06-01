b32112e6c70a49373d0f28323efd88511bc17087
kimardenmiller <kimardenmiller@gmail.com>
2014-06-01 11:26:41 -0700

    Cleanup

--- public/js/config.js
+++ public/js/config.js
@@ -8,13 +8,14 @@ require.config({
   baseUrl: "/js/",
 
   paths: {
+    "jquery": "/lib/jquery-2.1.0.min",
+    "jquery-slider": "/lib/jquery-ui-1.10.4.slider",
     "d3": "/lib/d3.v3.3.2.min",
     "tpl": "/lib/tpl",
     "underscore": "/lib/lodash.min",
     "highcharts": "/lib/highcharts-all",
     "numeral": "/lib/numeral.min"
-//    "jquery-slider": "/lib/jquery-ui-1.10.4.slider"
-//    "jquery": "/lib/jquery-2.1.0.min",
+
 //    "backbone": "/lib/backbone-min",
 //    "bootstrap": "/lib/bootstrap_232/js/bootstrap.min",
   },
